1d57fb2371b48bff3ed42422b518ea7da7249f69
Dante De Ruwe <dantederuwe@gmail.com>
2021-02-25 16:37:27 +0100

    Clean, translate and organize files

--- thesis/compile.sh
+++ thesis/compile.sh
@@ -1,4 +1,4 @@
 cd ../proposal
 latexmk DeRuwe_Dante_BachelorThesisProposal.tex -synctex=1 -interaction=nonstopmode -file-line-error -pdf
 cd ../thesis
-latexmk bachproef-tin.tex -synctex=1 -interaction=nonstopmode -file-line-error -pdf
+latexmk DeRuwe_Dante_BachelorThesis.tex -synctex=1 -interaction=nonstopmode -file-line-error -pdf
