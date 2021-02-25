8e4faa2ec4826ecc0b46f26ee5b16f3c5e822305
Dante De Ruwe <dantederuwe@gmail.com>
2021-02-25 15:16:23 +0100

    Initial setup of latex files for the main thesis

---
+++ thesis/compile.sh
@@ -0,0 +1,4 @@
+cd ../proposal
+latexmk DeRuwe_Dante_BachelorThesisProposal.tex -synctex=1 -interaction=nonstopmode -file-line-error -pdf
+cd ../thesis
+latexmk bachproef-tin.tex -synctex=1 -interaction=nonstopmode -file-line-error -pdf
