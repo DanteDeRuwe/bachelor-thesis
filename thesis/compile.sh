e6ee7c3e4c84212dc228efbfd40ce462f6056ade
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-28 12:15:13 +0200

    Add glossaries setup

--- thesis/compile.sh
+++ thesis/compile.sh
@@ -2,3 +2,5 @@ cd ../proposal
 latexmk DeRuwe_Dante_BachelorThesisProposal.tex -synctex=1 -interaction=nonstopmode -file-line-error -pdf
 cd ../thesis
 latexmk DeRuwe_Dante_BachelorThesis.tex -synctex=1 -interaction=nonstopmode -file-line-error -pdf
+makeglossaries DeRuwe_Dante_BachelorThesis
+latexmk DeRuwe_Dante_BachelorThesis.tex -synctex=1 -interaction=nonstopmode -file-line-error -pdf
